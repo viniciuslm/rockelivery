bf5ae92d8f4939316f46e8be7a1f4f9680271f55
Vinicius Moreira <viniciuslm@gmail.com.br>
2022-04-28 06:38:48 -0300

    migrate

--- priv/repo/migrations/20220428012355_create_users_table.exs
+++ priv/repo/migrations/20220428012355_create_users_table.exs
@@ -2,6 +2,20 @@ defmodule Rockelivery.Repo.Migrations.CreateUsersTable do
   use Ecto.Migration
 
   def change do
+    create table(:users, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :address, :string
+      add :age, :integer
+      add :cep, :string
+      add :cpf, :string
+      add :email, :string
+      add :password_hash, :string
+      add :name, :string
 
+      timestamps()
+    end
+
+    create unique_index(:users, [:cpf])
+    create unique_index(:users, [:email])
   end
 end
