4fb504f9acd247cc3bea9c11a6397de25419084a
Vinicius Moreira <viniciuslm@gmail.com.br>
2022-04-27 22:25:32 -0300

    setup inicial

---
+++ lib/rockelivery_web/router.ex
@@ -0,0 +1,28 @@
+defmodule RockeliveryWeb.Router do
+  use RockeliveryWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", RockeliveryWeb do
+    pipe_through :api
+
+    get "/", WelcomeController, :index
+  end
+
+  # coveralls-ignore-start
+  # Enables the Swoosh mailbox preview in development.
+  #
+  # Note that preview only shows emails that were sent by the same
+  # node running the Phoenix server.
+  if Mix.env() == :dev do
+    scope "/dev" do
+      pipe_through [:fetch_session, :protect_from_forgery]
+
+      forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
+  # coveralls-ignore-stop
+end
